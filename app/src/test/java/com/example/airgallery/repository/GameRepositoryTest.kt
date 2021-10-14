fb36a85cc8127209b1117b0b6c631865f6a703aa
Abid <abidcs9@gmail.com>
2021-10-14 23:41:03 +0500

    Wrote some Unit Tests

---
+++ app/src/test/java/com/example/airgallery/repository/GameRepositoryTest.kt
@@ -0,0 +1,4 @@
+package com.example.airgallery.repository
+
+class GameRepositoryTest {
+}
---
+++ app/src/test/java/com/example/airgallery/utils/Extensions.kt
@@ -0,0 +1,5 @@
+package com.example.airgallery.utils
+
+import org.mockito.Mockito
+
+fun <T> anyValue(): T = Mockito.any<T>()
