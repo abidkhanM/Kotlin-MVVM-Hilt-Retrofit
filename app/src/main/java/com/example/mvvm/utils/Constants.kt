c91efb1ee27a046f426a06b7ef68f84c8f4f97bc
abidkhanM <61864189+abidkhanM@users.noreply.github.com>
2023-03-29 00:26:54 +0500

    API key

--- app/src/main/java/com/example/mvvm/utils/Constants.kt
+++ app/src/main/java/com/example/mvvm/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.mvvm.utils
 
 object Constants {
     const val BASE_URL = "https://api.rawg.io/api/"
-    const val RAWG_API_KEY = "720346b66d2445c497966345f5caf362"
+    const val RAWG_API_KEY = "YourAPIKeyGoesHere"
 }
